2dc78dff0219d2930dc3c938fdb7e19b58685658
Prahar Jokhakar <88578584+Jethalal1234@users.noreply.github.com>
2025-04-18 00:35:47 +0530

    Delete Lab-8/shard.js

--- Lab-8/shard.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require("discord.js");
-const {TOKEN} = require("./config.json")
-
-const manager = new ShardingManager('./index.js', { token: TOKEN, totalShards: 2 });
-manager.on("shardCreate", (shard) => {
-    console.log(`Launched shard #${shard.id}`);
-});
-manager.spawn({ amount: manager.totalShards, timeout: -1 });
